265e3d81c2e0bdf48415748996a629cb2b7d5d7b
Partha Jyoti Pandey <parthajyoti.pandey45@gmail.com>
2023-06-08 01:48:10 +0530

    project setted up

---
+++ app.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+
+
+const app = express();
+
+const PORT = process.env.PORT || 3000; 
+const mongourl = process.env.MONGO_URL;
+
+mongoose.connect(mongourl,{
+    useNewUrlParser: true
+})
+.then((res)=> console.log("db connected"))
+.catch((err)=> console.log(err));
+
+
+
+
+app.listen(PORT, ()=> {
+    console.log(`Server is running on port ${PORT}`)
+})
+
